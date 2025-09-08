a952e55aa1ed2607ad1725ab7a9caed1079054da
Jason Yin <j.yin@ledvance.com>
2025-09-08 09:13:01 +0800

    处理涂鸦非正式版SDK会提示 Testing only弹窗问题

--- core/tuya/src/main/java/com/ledvance/tuya/TuyaSdkManager.kt
+++ core/tuya/src/main/java/com/ledvance/tuya/TuyaSdkManager.kt
@@ -5,8 +5,11 @@ import android.content.Context
 import android.util.Log
 import com.ledvance.tuya.impl.BizBundleFamilyServiceImpl
 import com.ledvance.tuya.utils.AppConfig
+import com.ledvance.utils.CoroutineScopeUtils
 import com.ledvance.utils.extensions.isDebuggable
+import com.ledvance.utils.extensions.tryCatch
 import com.thingclips.sdk.core.PluginManager
+import com.thingclips.security.prompt.PreferencesManager
 import com.thingclips.smart.activator.scan.qrcode.ScanManager
 import com.thingclips.smart.android.common.utils.L
 import com.thingclips.smart.commonbiz.bizbundle.family.api.AbsBizBundleFamilyService
@@ -16,6 +19,7 @@ import com.thingclips.smart.interior.api.IThingHomePlugin
 import com.thingclips.smart.optimus.sdk.ThingOptimusSdk
 import com.thingclips.smart.thingpackconfig.PackConfig
 import com.thingclips.smart.wrapper.api.ThingWrapper
+import kotlinx.coroutines.Dispatchers
 import timber.log.Timber
 
 
@@ -29,6 +33,7 @@ object TuyaSdkManager {
     private const val TAG = "TuyaSdkManager"
     private const val THING_TAG = "Thing"
     internal fun init(application: Application) {
+        disableSecurityPrompt(application)
         PackConfig.addValueDelegate(AppConfig::class.java)
         L.setLogInterception(if (application.isDebuggable()) Log.DEBUG else Log.INFO) { level, tag, msg ->
             Timber.tag(THING_TAG).log(priority = level, "$tag $msg")
@@ -70,6 +75,18 @@ object TuyaSdkManager {
 
     internal fun onLogout(context: Context) {
         ThingWrapper.onLogout(context)
+    }
+
+    internal fun disableSecurityPrompt(context: Context) {
+        CoroutineScopeUtils.launch(Dispatchers.IO) {
+            tryCatch {
+                // 关闭涂鸦application is for testing only提示弹窗，只会在非正式版SDK会弹出
+                PreferencesManager(context).putLong(
+                    "thing_security_prompt_key",
+                    System.currentTimeMillis()
+                )
+            }
+        }
     }
 
 }
